8e147d2886a68b51ee7c87a7db1c88fc2fccdbc0
LeoSanchez22 <lsanchezr29@upao.edu.pe>
2025-01-25 22:04:32 -0500

    feature(backend): Login y register avance 2

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_app (name, email, contrasena)
+VALUES
+    ('Leonardo', 'leonardo@gmail.com', '12345'),
+    ('Maria', 'maria@hotmail.com', 'password123'),
+    ('Carlos', 'carlos@yahoo.com', 'qwerty'),
+    ('Ana', 'ana@gmail.com', 'admin2023');
